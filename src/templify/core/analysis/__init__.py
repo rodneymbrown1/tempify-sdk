0aa4258bbd14aac92597a53446559df19611ce2a
Rodney <rodneymbrown@Mac.lan>
2025-09-09 18:45:06 -0400

    updated __init__ s

---
+++ src/templify/core/analysis/__init__.py
@@ -0,0 +1,10 @@
+from .features import LineFeatures, extract_line_features, batch_extract_features
+from .plaintext_context import PlaintextContext, build_line_context
+
+__all__ = [
+    "LineFeatures",
+    "extract_line_features",
+    "batch_extract_features",
+    "PlaintextContext",
+    "build_plaintext_context",
+]
---
+++ src/templify/core/analysis/detectors/__init__.py
@@ -0,0 +1,66 @@
+# src/templify/core/analysis/detectors/__init__.py
+"""
+Unified public API for the detectors package.
+
+Import from here in tests and calling code, e.g.:
+
+    from templify.core.analysis.detectors import (
+        ExactMatcher, find_exact_matches,
+        build_regexes_from_phrases, regex_search_lines,
+        HeuristicClassifier, classify_lines,
+        SemanticClassifier, semantic_classify,
+        RegexDetection, HeuristicPrediction, SemanticPrediction,
+        coerce_to_lines,
+    )
+"""
+
+from .exact_matcher import (
+    ExactMatcher,
+    find_exact_matches,
+    Detection as ExactDetection,  # alias if you want it, optional
+)
+
+from .regex_maker import (
+    build_regexes_from_phrases,
+    regex_search_lines,
+    RegexDetection,
+)
+
+from .heuristic_classifier import (
+    HeuristicClassifier,
+    classify_lines,
+    HeuristicPrediction,
+)
+
+from .semantic_classifier import (
+    SemanticClassifier,
+    semantic_classify,
+    SemanticPrediction,
+)
+
+from .utils import coerce_to_lines
+
+__all__ = [
+    # Exact matcher
+    "ExactMatcher",
+    "find_exact_matches",
+    "ExactDetection",
+
+    # Regex maker
+    "build_regexes_from_phrases",
+    "regex_search_lines",
+    "RegexDetection",
+
+    # Heuristic classifier
+    "HeuristicClassifier",
+    "classify_lines",
+    "HeuristicPrediction",
+
+    # Semantic classifier
+    "SemanticClassifier",
+    "semantic_classify",
+    "SemanticPrediction",
+
+    # Shared util
+    "coerce_to_lines",
+]
